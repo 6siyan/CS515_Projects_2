4258ac5b48f1853423b42ae93b15431cf9687af4
Yuqi <ydong28@stevens.edu>
2023-04-13 21:18:17 -0400

    add subtract multiple divide

--- live.py
+++ live.py
@@ -28,6 +28,8 @@ def lex(s: str) -> list[token]:
     [token('sym', '!'), token('var', 'a'), token('sym', '||'), token('var', 'b'), token('sym', '&&'), token('var', 'c')]
     >>> lex('))\\t\\t(   !a')
     [token('sym', ')'), token('sym', ')'), token('sym', '('), token('sym', '!'), token('var', 'a')]
+    >>> lex('123 4.56')
+    [token('num', '123'), token('num', '4.56')]
     """
 
     tokens = []
@@ -49,9 +51,34 @@ def lex(s: str) -> list[token]:
             else:
                 tokens.append(token('var', word))
 
-            i = end
+        #     i = end
+        # elif s[i].isdigit():
+        #     end = i + 1
+        #     while end < len(s) and s[end].isdigit():
+        #         end += 1
+        #     if end < len(s) and s[end] == '.':
+        #         end += 1
+        #         while end < len(s) and s[end].isdigit():
+        #             end += 1
+        #         tokens.append(token('num', s[i:end]))
+        #     else:
+        #         tokens.append(token('num', s[i:end]))
+        #     i = end
+
         elif s[i] == '!':
             tokens.append(token('sym', '!'))
+            i += 1
+        elif s[i] == '+':
+            tokens.append(token('sym', '+'))
+            i += 1
+        elif s[i] == '-':
+            tokens.append(token('sym', '-'))
+            i += 1
+        elif s[i] == '*':
+            tokens.append(token('sym', '*'))
+            i += 1
+        elif s[i] == '/':
+            tokens.append(token('sym', '/'))
             i += 1
         elif s[i] == '(':
             tokens.append(token('sym', '('))
@@ -91,76 +118,44 @@ class ast():
 def parse(s: str) -> ast:
     ts = lex(s)
 
-    a, i = disj(ts, 0)
+    # a, i = disj(ts, 0)
+
+    a, i = add_subtract(ts, 0)
 
     if i != len(ts):
         raise SyntaxError(f"expected EOF, found {ts[i:]!r}")
 
     return a
 
-def disj(ts: list[token], i: int) -> tuple[ast, int]:
-    """
-    >>> parse('true || false')
-    ast('||', ast('val', True), ast('val', False))
-    """
+def add_subtract(ts: list[token], i: int) -> tuple[ast, int]:
     if i >= len(ts):
-        raise SyntaxError('expected conjunction, found EOF')
+        raise SyntaxError('expected add_subtract, found EOF')
 
-    lhs, i = conj(ts, i)
+    lhs, i = multiply_divide(ts, i)
 
-    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '||':
-        rhs, i = conj(ts, i+1)
-        lhs = ast('||', lhs, rhs)
+    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '+' or ts[i].val == '-':
+        flag_as = ts[i].val
+        rhs, i = multiply_divide(ts, i+1)
+        lhs = ast(flag_as, lhs, rhs)
 
     return lhs, i
 
-def conj(ts: list[token], i: int) -> tuple[ast, int]:
-    """
-    >>> parse('true && false')
-    ast('&&', ast('val', True), ast('val', False))
-    >>> parse('!x && (a && !false)')
-    ast('&&', ast('!', ast('var', 'x')), ast('&&', ast('var', 'a'), ast('!', ast('val', False))))
-    >>> parse('!x && a && !false')
-    ast('&&', ast('&&', ast('!', ast('var', 'x')), ast('var', 'a')), ast('!', ast('val', False)))
-    """
+
+def multiply_divide(ts: list[token], i: int) -> tuple[ast, int]:
     if i >= len(ts):
-        raise SyntaxError('expected conjunction, found EOF')
+        raise SyntaxError('expected multiply_divide, found EOF')
 
-    lhs, i = neg(ts, i)
+    lhs, i = atom_new(ts, i)
 
-    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '&&':
-        rhs, i = neg(ts, i+1)
-        lhs = ast('&&', lhs, rhs)
+    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '*' or ts[i].val == '/':
+        flag_md = ts[i].val
+        rhs, i = atom_new(ts, i+1)
+        lhs = ast(flag_md, lhs, rhs)
 
     return lhs, i
 
-def neg(ts: list[token], i: int) -> tuple[ast, int]:
-    """
-    >>> parse('! true')
-    ast('!', ast('val', True))
-    >>> parse('!! true')
-    ast('!', ast('!', ast('val', True)))
-    """
 
-    if i >= len(ts):
-        raise SyntaxError('expected negation, found EOF')
-
-    if ts[i].typ == 'sym' and ts[i].val == '!':
-        a, i = neg(ts, i+1)
-        return ast('!', a), i
-    else:
-        return atom(ts, i)
-
-def atom(ts: list[token], i: int) -> tuple[ast, int]:
-    """
-    >>> parse('x')
-    ast('var', 'x')
-    >>> parse('true')
-    ast('val', True)
-    >>> parse('(((false)))')
-    ast('val', False)
-    """
-
+def atom_new(ts: list[token], i: int) -> tuple[ast, int]:
     t = ts[i]
 
     if t.typ == 'var':
@@ -168,17 +163,100 @@ def atom(ts: list[token], i: int) -> tuple[ast, int]:
     elif t.typ == 'kw' and t.val in ['true', 'false']:
         return ast('val', t.val == 'true'), i + 1
     elif t.typ == 'sym' and t.val == '(':
-        a, i = disj(ts, i + 1)
+        a, i = add_subtract(ts, i + 1)
 
         if i >= len(ts):
             raise SyntaxError(f'expected right paren, got EOF')
 
         if not (ts[i].typ == 'sym' and ts[i].val == ')'):
             raise SyntaxError(f'expected right paren, got "{ts[i]}"')
-        
+
         return a, i + 1
 
     raise SyntaxError(f'expected atom, got "{ts[i]}"')
+
+
+# def disj(ts: list[token], i: int) -> tuple[ast, int]:
+#     """
+#     >>> parse('true || false')
+#     ast('||', ast('val', True), ast('val', False))
+#     """
+#     if i >= len(ts):
+#         raise SyntaxError('expected conjunction, found EOF')
+#
+#     lhs, i = conj(ts, i)
+#
+#     while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '||':
+#         rhs, i = conj(ts, i+1)
+#         lhs = ast('||', lhs, rhs)
+#
+#     return lhs, i
+#
+# def conj(ts: list[token], i: int) -> tuple[ast, int]:
+#     """
+#     >>> parse('true && false')
+#     ast('&&', ast('val', True), ast('val', False))
+#     >>> parse('!x && (a && !false)')
+#     ast('&&', ast('!', ast('var', 'x')), ast('&&', ast('var', 'a'), ast('!', ast('val', False))))
+#     >>> parse('!x && a && !false')
+#     ast('&&', ast('&&', ast('!', ast('var', 'x')), ast('var', 'a')), ast('!', ast('val', False)))
+#     """
+#     if i >= len(ts):
+#         raise SyntaxError('expected conjunction, found EOF')
+#
+#     lhs, i = neg(ts, i)
+#
+#     while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '&&':
+#         rhs, i = neg(ts, i+1)
+#         lhs = ast('&&', lhs, rhs)
+#
+#     return lhs, i
+#
+# def neg(ts: list[token], i: int) -> tuple[ast, int]:
+#     """
+#     >>> parse('! true')
+#     ast('!', ast('val', True))
+#     >>> parse('!! true')
+#     ast('!', ast('!', ast('val', True)))
+#     """
+#
+#     if i >= len(ts):
+#         raise SyntaxError('expected negation, found EOF')
+#
+#     if ts[i].typ == 'sym' and ts[i].val == '!':
+#         a, i = neg(ts, i+1)
+#         return ast('!', a), i
+#     else:
+#         return atom(ts, i)
+#
+# def atom(ts: list[token], i: int) -> tuple[ast, int]:
+#     """
+#     >>> parse('x')
+#     ast('var', 'x')
+#     >>> parse('true')
+#     ast('val', True)
+#     >>> parse('(((false)))')
+#     ast('val', False)
+#     """
+#
+#     t = ts[i]
+#
+#     if t.typ == 'var':
+#         return ast('var', t.val), i+1
+#     elif t.typ == 'kw' and t.val in ['true', 'false']:
+#         return ast('val', t.val == 'true'), i + 1
+#     elif t.typ == 'sym' and t.val == '(':
+#         a, i = disj(ts, i + 1)
+#
+#         if i >= len(ts):
+#             raise SyntaxError(f'expected right paren, got EOF')
+#
+#         if not (ts[i].typ == 'sym' and ts[i].val == ')'):
+#             raise SyntaxError(f'expected right paren, got "{ts[i]}"')
+#
+#         return a, i + 1
+#
+#     raise SyntaxError(f'expected atom, got "{ts[i]}"')
 
 # INTERPRETER
 
@@ -199,5 +277,7 @@ def interp(a: ast, env: set[str]) -> bool:
         return interp(a.children[0], env) and interp(a.children[1], env)
     elif a.typ == '||':
         return interp(a.children[0], env) or interp(a.children[1], env)
-    
+
     raise SyntaxError(f'unknown operation {a.typ}')
+
+print(parse('a + b - c * d'))
